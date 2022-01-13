fd6466297946dd667ee42a213bf4f7b11d9f31a8
Ahmed Fouad <ahmedfouad@Ahmeds-MacBook-Pro.local>
2022-01-13 19:30:08 +0200

    adding premission request and location logic and render componets for different cases

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  View,
+  ActivityIndicator,
+  SafeAreaView,
+  LogBox,
+} from "react-native";
+import { useEffect, useState } from "react";
+import * as Location from "expo-location";
+import Run from "./components/Run";
 export default function App() {
+  const [appState, setAppState] = useState({
+    ready: false,
+    latitude: null,
+    longitude: null,
+  });
+
+  useEffect(() => {
+    LogBox.ignoreAllLogs();
+
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        return alert("Permission to access location was denied");
+      }
+      let location = await Location.getCurrentPositionAsync({});
+
+      setAppState({
+        ready: true,
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      });
+    })();
+  }, []);
+
+  if (!appState.ready) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={"large"} color={"white"} />
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <StatusBar
+          animated={true}
+          backgroundColor="#61dafb"
+          barStyle={"default"}
+        />
+        <Run
+          distance={1000}
+          {...{ latitude: appState.latitude, longitude: appState.longitude }}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#29252b",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
